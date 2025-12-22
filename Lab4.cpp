b69fe5003cd47a1f046382e4aaabd31215c4aada
firePC <61236983+firePC@users.noreply.github.com>
2025-12-23 01:03:42 +0300

    Update Lab4_var3.cpp

--- Lab4.cpp
+++ Lab4.cpp
@@ -1,30 +1,31 @@
 #include <iostream>
 #include <string>
 
-
 using namespace std;
+
 
 class ModelWindow {
 private:
-    string title;
-    int x, y;          
+    string title;      
+    int x, y;         
     int width, height; 
-    string color;
-    bool visible;
-    bool framed;
+    string color;      
+    bool visible;     
+    bool framed;       
     bool isWithinScreen(int newX, int newY, int newWidth, int newHeight) const;
 
 public:
-    ModelWindow();
-    ModelWindow(string t, int posX, int posY, int w, int h, string c, bool v, bool f);
-    ModelWindow(const ModelWindow& other);
+    ModelWindow(); 
+    ModelWindow(string t, int posX, int posY, int w, int h, string c, bool v, bool f); 
+    ModelWindow(const ModelWindow& other); 
 
-    void moveHorizontal(int dx);
-    void moveVertical(int dy);
-    void resize(int newWidth, int newHeight);
-    void changeColor(const string& newColor);
-    void toggleVisibility();
-    void toggleFrame();
+    void moveHorizontal(int dx); 
+    void moveVertical(int dy);   
+    void resize(int newWidth, int newHeight); 
+    void changeColor(const string& newColor); 
+    void toggleVisibility(); 
+    void toggleFrame();      
+
 
     bool isVisible() const;
     bool hasFrame() const;
@@ -35,27 +36,33 @@ public:
     int getWidth() const;
     int getHeight() const;
 
-    ModelWindow operator+(const ModelWindow& other) const;
-    bool operator==(const ModelWindow& other) const;
-    bool operator!=(const ModelWindow& other) const;
+
+    ModelWindow operator+(const ModelWindow& other) const; 
+    bool operator==(const ModelWindow& other) const; 
+    bool operator!=(const ModelWindow& other) const; 
+
 
     friend ostream& operator<<(ostream& os, const ModelWindow& win);
     friend istream& operator>>(istream& is, ModelWindow& win);
 };
 
+
+
 bool ModelWindow::isWithinScreen(int newX, int newY, int newWidth, int newHeight) const {
-    const int screenWidth = 1920;
-    const int screenHeight = 1080;
+    const int screenWidth = 1920;   
+    const int screenHeight = 1080;  
     return newX >= 0 && newY >= 0 &&
         (newX + newWidth) <= screenWidth &&
         (newY + newHeight) <= screenHeight;
 }
+
 
 ModelWindow::ModelWindow()
     : title("Untitled"), x(0), y(0), width(100), height(100),
     color("white"), visible(true), framed(true) {
 }
 
+// Конструктор
 ModelWindow::ModelWindow(string t, int posX, int posY, int w, int h,
     string c, bool v, bool f)
     : title(t), x(posX), y(posY), width(w), height(h),
@@ -66,13 +73,14 @@ ModelWindow::ModelWindow(string t, int posX, int posY, int w, int h,
     }
 }
 
-
+// Конструктор копирования
 ModelWindow::ModelWindow(const ModelWindow& other)
     : title(other.title), x(other.x), y(other.y),
     width(other.width), height(other.height),
     color(other.color), visible(other.visible),
     framed(other.framed) {
 }
+
 
 void ModelWindow::moveHorizontal(int dx) {
     if (isWithinScreen(x + dx, y, width, height)) {
@@ -82,6 +90,7 @@ void ModelWindow::moveHorizontal(int dx) {
         cout << "Невозможно сдвинуть по горизонтали: выход за границы.\n";
     }
 }
+
 
 void ModelWindow::moveVertical(int dy) {
     if (isWithinScreen(x, y + dy, width, height)) {
@@ -102,63 +111,46 @@ void ModelWindow::resize(int newWidth, int newHeight) {
     }
 }
 
+// Изменение цвета окна
 void ModelWindow::changeColor(const string& newColor) {
     color = newColor;
 }
 
+// Переключение видимости окна
 void ModelWindow::toggleVisibility() {
     visible = !visible;
 }
 
+// Переключение рамки окна
 void ModelWindow::toggleFrame() {
     framed = !framed;
 }
 
-bool ModelWindow::isVisible() const {
-    return visible;
-}
+// Геттеры
+bool ModelWindow::isVisible() const { return visible; }
+bool ModelWindow::hasFrame() const { return framed; }
+string ModelWindow::getColor() const { return color; }
+string ModelWindow::getTitle() const { return title; }
+int ModelWindow::getX() const { return x; }
+int ModelWindow::getY() const { return y; }
+int ModelWindow::getWidth() const { return width; }
+int ModelWindow::getHeight() const { return height; }
 
-bool ModelWindow::hasFrame() const {
-    return framed;
-}
-
-string ModelWindow::getColor() const {
-    return color;
-}
-
-string ModelWindow::getTitle() const {
-    return title;
-}
-
-int ModelWindow::getX() const {
-    return x;
-}
-
-int ModelWindow::getY() const {
-    return y;
-}
-
-int ModelWindow::getWidth() const {
-    return width;
-}
-
-int ModelWindow::getHeight() const {
-    return height;
-}
-
+// Объединение окон
 ModelWindow ModelWindow::operator+(const ModelWindow& other) const {
-    int newX = (x + other.x) / 2;
-    int newY = (y + other.y) / 2;
-    int newWidth = width + other.width;
-    int newHeight = height + other.height;
-    string newColor = (color == other.color) ? color : "mixed";
-    bool newVisible = visible && other.visible;
-    bool newFramed = framed && other.framed;
-    string newTitle = title + "+" + other.title;
+    int newX = (x + other.x) / 2;                   
+    int newY = (y + other.y) / 2;                    
+    int newWidth = width + other.width;             
+    int newHeight = height + other.height;           
+    string newColor = (color == other.color) ? color : "mixed"; 
+    bool newVisible = visible && other.visible;      
+    bool newFramed = framed && other.framed;        
+    string newTitle = title + "+" + other.title;    
 
     return ModelWindow(newTitle, newX, newY, newWidth, newHeight, newColor, newVisible, newFramed);
 }
 
+// Оператор сравнения на равенство
 bool ModelWindow::operator==(const ModelWindow& other) const {
     return (x == other.x && y == other.y &&
         width == other.width && height == other.height &&
@@ -166,10 +158,12 @@ bool ModelWindow::operator==(const ModelWindow& other) const {
         framed == other.framed && title == other.title);
 }
 
+// Сравнение
 bool ModelWindow::operator!=(const ModelWindow& other) const {
     return !(*this == other);
 }
 
+// Вывода 
 ostream& operator<<(ostream& os, const ModelWindow& win) {
     os << "Окно: \"" << win.title << "\"\n";
     os << "Позиция: (" << win.x << ", " << win.y << ")\n";
@@ -180,6 +174,7 @@ ostream& operator<<(ostream& os, const ModelWindow& win) {
     return os;
 }
 
+// Ввод 
 istream& operator>>(istream& is, ModelWindow& win) {
     cout << "Введите заголовок: ";
     getline(is, win.title);
@@ -197,9 +192,11 @@ istream& operator>>(istream& is, ModelWindow& win) {
 }
 
 int main() {
-    setlocale(LC_ALL, "Russian");
+    setlocale(LC_ALL, "Russian"); 
+
     ModelWindow win1("Main", 100, 100, 300, 200, "blue", true, true);
     ModelWindow win2("Dialog", 200, 200, 150, 150, "green", true, false);
+
     ModelWindow win3 = win1 + win2;
 
     cout << win1 << endl;
@@ -209,9 +206,9 @@ int main() {
     cout << "win1 == win2? " << (win1 == win2 ? "да" : "нет") << endl;
     cout << "win1 != win2? " << (win1 != win2 ? "да" : "нет") << endl;
 
-    win1.moveHorizontal(50);
-    win2.toggleVisibility();
-    win3.changeColor("red");
+    win1.moveHorizontal(50);      
+    win2.toggleVisibility();      
+    win3.changeColor("red");     
 
     cout << "\nПосле изменений:\n";
     cout << win1 << endl;
